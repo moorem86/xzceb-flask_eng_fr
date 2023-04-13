42494016e18619ab3d789b60426f999392ff7a9d
Megan <moorem86@gmail.com>
2023-04-13 19:15:43 -0400

    step 1

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,36 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def englishToFrench(englishText):
+    frenchText = language_translator.translate(
+    text='Hello, how are you today?',
+    model_id='en-es').get_result()
+    
+    print(json.dumps(frenchText, indent=2, ensure_ascii=False))
+
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    englishText = language_translator.translate(
+    text='Hello, how are you today?',
+    model_id='en-es').get_result()
+    
+    print(json.dumps(englishText, indent=2, ensure_ascii=False))
+
+    return englishText
